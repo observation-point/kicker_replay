83fe0c59293b5677a6d3619dac13d41a3d5336b8
Pom4H <1@rom4.ru>
2019-05-19 16:05:40 +0300

    fix: config builder

--- src/components/container/initInfContainer.ts
+++ src/components/container/initInfContainer.ts
@@ -1,26 +1,18 @@
 import { Type } from '@diType';
 import { Container } from 'inversify';
 import { getConfigFacotry } from '@framework';
-import { RecordConfig, StreamConfig } from '@config';
+import { RecorderConfig } from '@config';
 
 import { RecordService } from '../../inf/record/RecordService';
 import { StreamService } from '../../inf/stream/StreamService';
 
-// import { IServiceDiscovery } from '../restClient/IServiceDiscovery';
-// import { ServiceDiscovery } from '../restClient/ServiceDiscovery';
-
 export async function initInfContainer(container: Container, _options: { envName: string; }): Promise<void> {
   const configFacotry = getConfigFacotry(container);
-  // const envChecker = new EnvironmentChecker(process.env[options.envName] as string);
-
-  // envChecker.isDev ?
-  // container.bind<IServiceDiscovery>(Type.ServiceDiscovery)
-  //   .toConstantValue(new ServiceDiscovery(configFacotry.create(ServiceConfig) as any)) : null;
 
   container.bind<RecordService>(Type.RecordService)
-    .toConstantValue(new RecordService(configFacotry.create(RecordConfig)));
+    .toConstantValue(new RecordService(configFacotry.create(RecorderConfig)));
 
   container.bind<StreamService>(Type.StreamService)
-    .toConstantValue(await new StreamService(configFacotry.create(StreamConfig)).init());
+    .toConstantValue(await new StreamService(configFacotry.create(RecorderConfig)).init());
 
 }
